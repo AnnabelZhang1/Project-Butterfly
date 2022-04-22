b57b03eda0c6da0239ea4d1a541301b43adbbca6
AnnabelZhang1 <annabel.zhang1@gmail.com>
2022-04-21 22:25:38 -0400

    added buttons and sized canvas

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template, redirect, session, jsonify
+
+app = Flask(__name__)
+app.secret_key = "butterfly"
+
+@app.route("/", methods=['GET', 'POST'])
+def welcome():
+	return render_template("index.html")
+
+if __name__ == "__main__": #false if this file imported as module
+	#enable debugging, auto-restarting of server when this file is modified
+	app.debug = True
+	app.run()
---
+++ app/static/js/animate.js
@@ -0,0 +1,33 @@
+
+var c = document.getElementById('garden');
+var dotButton = document.getElementById('buttonCircle');
+
+//prepare to interact with canvas in 2D
+var ctx = c.getContext("2d")
+var ctx2 = c.getContext("2d")
+
+var requestID;
+
+
+var clear = (e) => {
+  console.log("clear invoked...")
+  ctx.clearRect(0, 0, c.clientWidth, c.clientHeight);
+};
+
+
+var drawFlowers = () => {
+    console.log("drawFlowers invoked...")
+
+}
+
+let butterfly = document.createElement("img");
+butterfly.src = '../static/img/demob.png';
+
+var drawButterfly = () => {
+  console.log("drawButterfly invoked...")
+
+  ctx.drawImage(butterfly, 0, 0, 300, 300);
+
+}
+
+dotButton.addEventListener("click", drawButterfly);
